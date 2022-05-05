6114626aa92b7fa09b060066b415010a62efe2d2
TJ Rice <49173478+ricetj@users.noreply.github.com>
2022-05-04 21:44:27 -0400

    Update values.sql

--- DB/testdata/values.sql
+++ DB/testdata/values.sql
@@ -1,1 +1,29 @@
 
+INSERT INTO accounts VALUES 
+(001, 'John', 'Smith', 'jsmith', 123);
+INSERT INTO accounts VALUES 
+(002, 'Lin', 'Xiao', 'lxiao', 888);
+INSERT INTO accounts VALUES 
+(003, 'Cho', 'Lee', 'clee', 678);
+INSERT INTO accounts VALUES 
+(004, 'Maria', 'Perez', 'mperez', 987);
+
+
+INSERT INTO manager VALUES 
+('Sophia', 'Kim');
+INSERT INTO manager VALUES 
+('Joshua', 'Thybony');
+INSERT INTO manager VALUES 
+('Xiaoyi', 'Zhu');
+INSERT INTO manager VALUES 
+('Terence', 'Rice');
+
+
+INSERT INTO menu VALUES 
+(11, 'Tomato soup', 'Taste like the soup my mother would make!');
+INSERT INTO menu VALUES 
+(22, 'House Salad', 'Seemed a bit pricy for this kind of salad');
+INSERT INTO menu VALUES 
+(33, 'Tagliatelle', 'I almost ordered another one it was so good');
+INSERT INTO menu VALUES 
+(44, 'Wedding soup', 'Died and went to heaven after eating this');
